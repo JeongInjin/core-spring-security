512b734b8f506ee38fbd0d2e751c1c6e93f95977
injinjeong <snowwwwww@naver.com>
2022-01-23 15:31:55 +0900

    SecurityContextLogoutHandler 를 통한 logout 추가

--- src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
+++ src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
@@ -1,7 +1,13 @@
 package io.security.corespringsecurity.controller.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -9,5 +15,19 @@ public class LoginController {
     @GetMapping("/login")
     public String login() {
         return "user/login/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null) {
+            //해당 클래스를 logout filter 에서 사용하여 로그아웃 처리를 한다.
+            //session 꺼내와서 무효화 시키고, SecurityContext 안에 있는 인증객체를 null 로 초기화 한 뒤, SecurityContext 객체도 삭제 한다.
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/login";
     }
 }
