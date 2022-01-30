73bcdeea6181690d61362c529ce15472869d68da
injinjeong <snowwwwww@naver.com>
2022-01-30 20:27:58 +0900

    충돌 해결

--- src/main/java/io/security/corespringsecurity/config/AppConfig.java
+++ src/main/java/io/security/corespringsecurity/config/AppConfig.java
@@ -1,11 +1,7 @@
 package io.security.corespringsecurity.config;
 
 import io.security.corespringsecurity.repository.ResourcesRepository;
-<<<<<<< HEAD
 import io.security.corespringsecurity.service.SecurityResourceService;
-=======
-import io.security.corespringsecurity.security.service.SecurityResourceService;
->>>>>>> e32bbfe (웹기반 인가처리 DB 연동 - FilterInvocationSecurityMetadataSource(2) test 완료)
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,11 +11,6 @@ public class AppConfig {
     @Bean
     public SecurityResourceService securityResourceService(ResourcesRepository resourcesRepository) {
         SecurityResourceService securityResourceService = new SecurityResourceService(resourcesRepository);
-<<<<<<< HEAD
-        
-=======
-
->>>>>>> e32bbfe (웹기반 인가처리 DB 연동 - FilterInvocationSecurityMetadataSource(2) test 완료)
         return securityResourceService;
     }
 }
--- src/main/java/io/security/corespringsecurity/security/factory/UrlResourcesMapFactoryBean.java
+++ src/main/java/io/security/corespringsecurity/security/factory/UrlResourcesMapFactoryBean.java
@@ -1,10 +1,6 @@
 package io.security.corespringsecurity.security.factory;
 
-<<<<<<< HEAD
 import io.security.corespringsecurity.service.SecurityResourceService;
-=======
-import io.security.corespringsecurity.security.service.SecurityResourceService;
->>>>>>> e32bbfe (웹기반 인가처리 DB 연동 - FilterInvocationSecurityMetadataSource(2) test 완료)
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.web.util.matcher.RequestMatcher;
@@ -27,10 +23,6 @@ public class UrlResourcesMapFactoryBean implements FactoryBean<LinkedHashMap<Req
         if (resourceMap == null) {
             init();
         }
-<<<<<<< HEAD
-=======
-
->>>>>>> e32bbfe (웹기반 인가처리 DB 연동 - FilterInvocationSecurityMetadataSource(2) test 완료)
         return resourceMap;
     }
 
@@ -45,10 +37,6 @@ public class UrlResourcesMapFactoryBean implements FactoryBean<LinkedHashMap<Req
 
     @Override
     public boolean isSingleton() {
-<<<<<<< HEAD
         return true;
-=======
-        return FactoryBean.super.isSingleton();
->>>>>>> e32bbfe (웹기반 인가처리 DB 연동 - FilterInvocationSecurityMetadataSource(2) test 완료)
     }
 }
