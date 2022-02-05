4cbe38b9d9e8419afeedb5957015176e2ffc8b5d
injinjeong <snowwwwww@naver.com>
2022-02-05 16:37:21 +0900

    권한인가처리 sample 추가 -> UserServiceImpl

--- src/main/java/io/security/corespringsecurity/service/impl/UserServiceImpl.java
+++ src/main/java/io/security/corespringsecurity/service/impl/UserServiceImpl.java
@@ -9,6 +9,7 @@ import io.security.corespringsecurity.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -86,5 +87,12 @@ public class UserServiceImpl implements UserService {
     @Override
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
+    }
+
+    //권한 인가처리
+    @Override
+    @Secured("ROLE_MANAGER")
+    public void order() {
+        System.out.println("order");
     }
 }
